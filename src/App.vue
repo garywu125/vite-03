baaae1df6ca061eaafd0ca5d00d47bd8c64793ea
garywu125 <garywu125@gmail.com>
2021-03-02 17:18:55 +0800

    add project msg

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite in vite-03 xx" />
+  <HelloWorld msg="Hello Vue 3 + Vite in project vite-03 " />
 </template>
 
 <script setup>
